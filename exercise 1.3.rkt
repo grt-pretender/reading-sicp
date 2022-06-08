25092053b24388508fe1f077ccba1e9674b748b5
Daria <dmetafrasis@gmail.com>
2022-06-08 22:52:29 +0700

    Add file

---
+++ exercise 1.3.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+; Exercise 1.3.
+; input: 3 numbers, return sum of squares (two bigger arguments)
+
+(define (major-square-sum a b c)
+  (cond ((and (< a b) (< a c)) (square-sum b c))
+        ((and (< b a) (< b c)) (square-sum a c))
+        (else (square-sum a b))))
+
+(define (square-sum x y)
+  (+ (* x x) (* y y)))
